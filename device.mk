415cc7cfa8b5f0ef0a116b6379c1ad86b144aac5
Han Sol Jin <hansol@hansol.ca>
2025-07-06 09:51:40 -0700

    caymanlm: add Soong namespace
    Change-Id: Id88addbf7d388a22db561f297b966cad1610cebf

--- device.mk
+++ device.mk
@@ -11,5 +11,9 @@ DEVICE_NAME := caymanlm
 # Inherit from the common device configuration.
 $(call inherit-product, device/lge/sm7250-common/sm7250-common.mk)
 
+# Soong namespace
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
 # Inherit from vendor makefiles.
 $(call inherit-product, vendor/lge/caymanlm/caymanlm-vendor.mk)
